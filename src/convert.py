77fbae9ac6f970557554cabd55f047649f4b2d72
denweeed <proverkamobi@gmail.com>
2023-01-22 22:11:06 +0200

    Initial commit

---
+++ src/convert.py
@@ -0,0 +1,15 @@
+def convert_string(d: tuple, delim: str = ", ") -> str:
+    return delim.join(d)
+
+
+if __name__ == '__main__':
+    tuple1 = ('1', '2', '3', '4', '5', '5', '5', '4', '2',)
+    str1 = convert_string(tuple1)
+    print(str1)
+
+'''The function convert_string is used to convert a tuple to a string
+    Parameters
+    ----------
+    delim : str, optional
+            separator
+'''
